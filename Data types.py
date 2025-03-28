c744c8e33b18f49509ecfd561e8905a6eaab091b
Abhigna1227 <abhigna1227@gmail.com>
2025-03-28 10:45:53 +0530

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,21 @@
+# Lets check the datatypes of different values
+a=5
+print("type of a: ", type(a))
+
+b=2.5
+print("type of b: ", type(b))
+
+c="coding"
+print("type of c:", type(c))
+
+d=True
+print("the type of d:", type(d))
+
+
+#type casting to convert the datatype of variables
+print("\n After type Casting .....")
+b=int(b)
+print(b)
+print("the type of d:", type(b))
+
+
