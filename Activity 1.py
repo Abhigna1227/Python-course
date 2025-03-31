df83d4800fd3eaae635f1c6ca19b104b1bd00313
Abhigna1227 <abhigna1227@gmail.com>
2025-03-31 10:30:42 +0530

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,17 @@
+#Storing values
+Tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#Finding total trees
+sum=Tree1+tree2+tree3+tree4+tree5
+print("The sum of the 5 trees is:" ,sum)
+
+#Finding the average of trees
+average=sum/5
+print("The average height of the trees is:",average)
+
+
+
