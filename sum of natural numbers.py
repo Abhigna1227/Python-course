5dde9677bfc90bae5b5db0bae901fe46e27d3095
Abhigna1227 <abhigna1227@gmail.com>
2025-04-09 10:26:13 +0530

    Add files via upload

---
+++ sum of natural numbers.py
@@ -0,0 +1,8 @@
+#Input the value of terms
+n=int(input("Enter the value of terms :"))
+sum=0 #initialise
+i=1 #initialise
+while i<=10: #loop will end from 1-n
+ sum=sum+i
+ i=i+1
+print("/nSum =",sum)
