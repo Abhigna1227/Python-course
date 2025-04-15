b1fe4573f755f64be3f3e0f362c9cfa7e5ce7894
Abhigna1227 <abhigna1227@gmail.com>
2025-04-15 14:28:18 +0530

    Add files via upload

---
+++ Lesson 3 ACP.py
@@ -0,0 +1,8 @@
+# Ram's message in lowercase
+message = "congratulations Aadhya for getting a gold meadel"
+
+# Convert the message to uppercase
+uppercase_message = str(message).upper()
+
+# Display the uppercase message
+print(uppercase_message)
