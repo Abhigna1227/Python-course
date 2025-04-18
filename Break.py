eb573e12f5768dfd4b404b411914c31b1f350d9a
Abhigna1227 <abhigna1227@gmail.com>
2025-04-18 10:34:05 +0530

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,12 @@
+#Take input fuom user
+a=input("Enter a word: ")
+#Program to check break keyword
+for i in a:#itrate for loop
+    if (i=='A'):#Condition 1
+     #display result
+     print("A is found")
+     break #Break statement
+    else:
+       #display result
+       print("A not found")
+    
