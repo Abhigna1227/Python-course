43b514f57e77aa0e5a9df47762711f40a96cd501
Abhigna1227 <abhigna1227@gmail.com>
2025-04-23 12:39:52 +0530

    Add files via upload

---
+++ Number swap.py
@@ -0,0 +1,9 @@
+print("This code is based on swapping three numbers")
+a=int(input("Enter your first number:"))
+b=int(input("Enter your second number:"))
+c=int(input("Enter the third number:"))
+print("Before swapping the number is:",a,b,c)
+a = b
+b = c
+c = a
+print("After swapping the number is:",a,b,c)
