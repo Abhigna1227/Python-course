b4fd26addb209ba51bde2494d1aacfd85db368fd
Abhigna1227 <abhigna1227@gmail.com>
2025-05-09 08:33:02 +0530

    Add files via upload

---
+++ Lesson 4 ACP.py
@@ -0,0 +1,10 @@
+import math
+
+# Ask the user for a number
+num = float(input("Enter a number to find its square root: "))
+
+# Calculate the square root
+sqrt = math.sqrt(num)
+
+# Print the result
+print(f"The square root of {num} is {sqrt}")
