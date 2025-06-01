b3dad00a9c97745cbcdedbde82393e276348cc6a
Abhigna1227 <abhigna1227@gmail.com>
2025-06-01 16:46:58 +0530

    Add files via upload

--- Activity 1.py
+++ Activity 1.py
@@ -1,17 +1,18 @@
-#Storing values
-Tree1 = 98
-tree2 = 94
-tree3 = 41
-tree4 = 95
-tree5 = 11
-
-#Finding total trees
-sum=Tree1+tree2+tree3+tree4+tree5
-print("The sum of the 5 trees is:" ,sum)
-
-#Finding the average of trees
-average=sum/5
-print("The average height of the trees is:",average)
-
-
-
+from tkinter import *
+root=Tk()
+root.title('number pad')
+root.geometry('250x300')
+num=[[9,8,7],[6,5,4],[3,2,1],['#',0,'*']]
+for i in range(4):
+    root.coloumnconfigure(i,weight=1,minsize=75)
+    root.rowconfigure(i,weight=1,minsize=50)
+    for i in range(4):
+        for j in range(3):
+            frame=Frame(master=root,
+                        relif=SUNKEN,
+                        borderwidth=1,
+                        bg=("#6b599a"))
+            frame.grid(row=i,coloumn=j,sticky="nsew")
+            label=label(master=frame,text=num[i][j],bg="#287196",font=('Arial',18))
+            label.pack(expand=True,fill='both,padx=5,paddy=5')
+root.mainloop()
